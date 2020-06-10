edcbe0627dd2023292b05f3f42c533a45d4222ee
MIhir Bagga <62807580+mihirbagga@users.noreply.github.com>
2020-06-10 13:29:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {View,Text, ScrollView,Button} from 'react-native'
+import MultipleList from './components/MultipleList'
+import Home from './components/Home'
+import Slider from './components/Carousel'
+import Register from './components/Register'
+import TopBrands from './components/TopBrands'
+import Horizontalcard from './components/sidecard'
+import Subcard from './components/Subcard'
+import Description from './components/Description'
+import StarRate from './components/Rating'
+import SignUp from './components/SignUp'
+import Login from './components/Login'
+import ShoppingBag from './components/ShoppingBag'
+import MainNavigation from './components/MainNavigation'
+import Splash from './components/Splash';
+import Menu from './components/menu'
+import MainNav from './components/MainNav'
+export default function Milt()
+{
+   console.disableYellowBox = true;
+   return(
+     
+          <MainNavigation/>
+          
+
+   )
+}
